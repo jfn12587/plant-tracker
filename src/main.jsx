067df65d190b82af5710651156d226a876c817ec
Josh Nedelka <jfn@amazon.com>
2026-08-02 21:36:39 -0400

    Add service worker for PWA install

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,7 @@ import { render } from 'preact';
 import { App } from './app.jsx';
 
 render(<App />, document.getElementById('app'));
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/plant-tracker/sw.js');
+}
